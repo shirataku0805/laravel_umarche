a1cfbfb02701fd708810f762f62d527ca1653db0
shirataku0805 <shirataku0805@gmail.com>
2022-07-03 21:45:15 +0900

    Store

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Owner; //エロクアント
-use Illuminate\Support\Facades\Hash; //QueryBuilder クエリビルダ
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB; //QueryBuilder クエリビルダ
 use Carbon\Carbon;
+
+
 
 class OwnersController extends Controller
 {
@@ -66,6 +68,20 @@ class OwnersController extends Controller
     public function store(Request $request)
     {
         //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:owners',
+            'password' => 'required|string|confirmed|min:8',
+        ]);
+        Owner::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with('message','オーナー登録を実施しました。');
     }
 
     /**
