5b43ab7a4ec17ca9e1a5ca695f4f6670488a90f4
shirataku0805 <shirataku0805@gmail.com>
2022-08-02 22:41:07 +0900

    product初期設定

--- routes/owner.php
+++ routes/owner.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use App\Http\Controllers\Owner\ShopController;
 use App\Http\Controllers\Owner\ImageController;
+use App\Http\Controllers\Owner\ProductController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -38,6 +39,9 @@ middleware('auth:owners')->group(function(){
 });
 
 Route::resource('images',ImageController::class)
+->middleware('auth:owners')->except(['show']);
+
+Route::resource('products',ProductController::class)
 ->middleware('auth:owners')->except(['show']);
 
 Route::get('/dashboard', function () {
